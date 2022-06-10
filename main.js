b27af7a2022dfa3bac86c262604b6fba03dcf3e9
MaksonGitHub25 <makson94021@gmail.com>
2022-06-10 15:15:11 +0300

    complete second task

--- main.js
+++ main.js
@@ -21,3 +21,68 @@ function createAndCheckRandomNum(min, max) {
 img.addEventListener('click', function () {
     document.location.reload();
 });
+
+console.log('============================================================');
+
+let num = '';
+const letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+// console.log('  X  X  X  X  X  X  X  X');
+// console.log('X  X  X  X  X  X  X  X');
+// console.log('  X  X  X  X  X  X  X  X');
+// console.log('X  X  X  X  X  X  X  X');
+// console.log('  X  X  X  X  X  X  X  X');
+// console.log('X  X  X  X  X  X  X  X');
+// console.log('  X  X  X  X  X  X  X  X');
+// console.log('X  X  X  X  X  X  X  X');
+
+
+let lineVar1 = '', lineVar2 = '';
+
+for (let i = 0; i<4; i++) {
+    // lineVar1 += '⬛  ⬜ ';
+    // lineVar2 += '⬜  ⬛ '; - чтоб цифры попадали под ячейки(и в стороке 54 добавить пробел в строчку)
+
+    lineVar1 += '⬛ ⬜ ';
+    lineVar2 += '⬜ ⬛ ';
+};
+
+// ⬜
+//⬛
+
+for (let i = 0; i<8; i++) {
+    num += `${i+1}  `;
+    
+    if (i % 2 == 0) {
+        console.log(`${letter[i]}  ${lineVar1}`);
+    } else {
+        console.log(`${letter[i]}  ${lineVar2}`);
+    }
+};
+console.log('    ' + num);
+
+
+
+
+
+
+
+
+
+
+// H/W
+
+// 1) Написать программу , которая выводит через клг числа от 1 до 23 . 
+// * Числа , что нацело делятся на 3 => Кирилл
+// * Числа , что нацело делятся на 5 => demiGod
+// * Числа , что нацело делятся на 3 и на 5 => Кирилл demiGod
+
+// 2) При помощи консоли вывести шахматную доску 8х8 
+// Пример : 
+// 				#  #  #  # 
+// 			#  #  #  # 
+// 				#  #  #  # 
+// 			#  #  #  # 
+
+// (Cycles : for , while )
+// (Cond : if / else ; switch ; ternary)
